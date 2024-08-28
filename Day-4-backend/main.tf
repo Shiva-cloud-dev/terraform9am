3ce726af7a841d3a780ced765c4152196486d3c4
shiva <angatashiva916@gmail.com>
2024-08-28 12:16:50 +0530

    adding the day-4

---
+++ Day-4-backend/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "terraform" {
+    ami = "ami-02b49a24cfb95941c"
+    instance_type = "t2.micro"
+    key_name = "terraform"
+    tags = {
+      Name = "Terra"
+    }
+  
+}
