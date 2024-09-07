561776e4aca3c475f06fc609170fd9b487adf64b
Shiva-cloud-dev <angatashiva916@gmail.com>
2024-09-07 17:49:10 +0530

    Update main.tf

--- Day-4-backend/main.tf
+++ Day-4-backend/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 resource "aws_instance" "terraform" {
-    ami = "ami-02b49a24cfb95941c"
+    ami = "ami-0e53db6fd757e38c7"
     instance_type = "t2.micro"
     key_name = "terraform"
     tags = {
