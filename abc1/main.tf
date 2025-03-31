b5380429ff694d58c50e3bb5e9ff537b8817e006
Shiva-cloud-dev <angatashiva916@gmail.com>
2025-03-31 15:11:38 +0530

    Update main.tf

--- abc1/main.tf
+++ abc1/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "name" {
     ami = "ami-071226ecf16aa7d96"
     instance_type = "t2.micro"
-    key_name = "terraform"
+    key_name = "project"
     tags = {
       Name = "Terra"
     }
